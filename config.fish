c3496eff9bee626a3154cd86eb88fa73aac09407
Edison Neto <eaneto@posteo.com>
2024-12-16 09:37:30 -0300

    update gpsup alias on fish config

--- config.fish
+++ config.fish
@@ -2,12 +2,18 @@ function git_branch
     git branch | sed -n '/\* /s///p'
 end
 
+function gpsup
+    git push --set-upstream origin $(git_branch)
+end
+
 # ENV variables
 export EDITOR=emacs
 export GIT_EDITOR=vim
 export PROJECTS=$HOME/projects
 export SCRIPTS=$PROJECTS/snippets/shell
 export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/go/bin"
 
 # Shortcuts
 abbr v "nvim"
@@ -27,7 +33,6 @@ abbr gd "git diff"
 abbr gl "git pull"
 abbr gf "git fetch"
 abbr gp "git push"
-abbr gpsup "git push --set-upstream origin $(git_branch)"
 abbr gpf "git push --force-with-lease"
 abbr gco "git checkout"
 abbr gcb "git checkout -b"
